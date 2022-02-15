a6984f3b81b76e18a4d36e6563621901e3d39153
David Yakubov <dyakubov@Davids-MacBook-Air.local>
2022-02-15 17:03:42 -0500

    removed a use state hook

--- src/components/Income.js
+++ src/components/Income.js
@@ -2,10 +2,6 @@ import { useState } from "react";
 
 function Income() {
 
-    const [state, setState] = useState({
-        
-    })
-
     return (
         <div className='income'>
             <p>Income</p>
