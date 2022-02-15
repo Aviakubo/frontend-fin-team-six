392a0b03c5d5f74ed2777ee07c0d18ff4ea20dbe
Ian Cannon <ian.cannon@protonmail.com>
2022-02-15 14:07:57 -0800

    Create models for income, expenses, assets, and debts

---
+++ models/debts.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const debtsSchema = new Schema({
+		name: {type: String},
+		amount: {type: Number},
+		category: {type: String},
+		}, {
+		timestamps: true
+});
+
+module.exports = mongoose.model('Debts', debtSchema);
---
+++ models/income.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const incomeSchema = new Schema({
+		name: {type: String},
+		amount: {type: Number},
+  frequency: {type: Number},
+		}, {
+		timestamps: true
+});
+
+module.exports = mongoose.model('Income', incomeSchema);
