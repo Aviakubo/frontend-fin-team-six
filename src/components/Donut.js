dee96e31fd59d15bafed65cef7d2e72ad6985ce0
David Yakubov <dyakubov@Davids-MacBook-Air.local>
2022-02-16 14:56:04 -0500

    donut chart

--- src/components/Donut.js
+++ src/components/Donut.js
@@ -1,22 +1,24 @@
 import React from 'react';
-import { PieChart, Pie} from 'recharts';
+import { PieChart, Pie, Legend, Tooltip, ResponsiveContainer } from 'recharts';
   
   
 const Donut = () => {
   
 // Sample data
 const data = [
-  {name: 'Income', amount: 400},
-  {name: 'Assets', amount: 700},
-  {name: 'Expenses', amount: 200},
-  {name: 'Debt', amount: 1000}
+  {name: 'Income', amount: 400, fill: 'green'},
+  {name: 'Assets', amount: 700, fill: 'green'},
+  {name: 'Expenses', amount: 200, fill: 'red'},
+  {name: 'Debt', amount: 500, fill: 'red'}
 ];
   
   
 return (
-        <PieChart width={700} height={700}>
-          <Pie data={data} dataKey="amount" outerRadius={250} innerRadius={150} fill="green" />
+    <>
+        <PieChart width={400} height={400}>
+          <Pie data={data} dataKey="amount" outerRadius={125} innerRadius={75} label/>
         </PieChart>
+    </>
 );
 }
   
