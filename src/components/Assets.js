02e36f586dea267007e360ac7828ebdf061dfa67
David Yakubov <dyakubov@Davids-MacBook-Air.local>
2022-02-16 21:58:08 -0500

    resolving errors and collating with nellies code or something idk

--- src/components/Assets.js
+++ src/components/Assets.js
@@ -3,22 +3,22 @@ import { useState } from 'react';
 function Assets({assetValues, handleAssetChange, removeAssetFields, addAssetFields}) {
   console.log(assetValues)
     return (
-      
-        <form className="formSteps" onSubmit={handleSubmit}>
-           <span className="steps">Step 4 |</span> Assets
-          {formValues.map((element, index) => (
+        
+        <form className='formSteps'>
+            Assets
+          {assetValues.map((element, index) => (
             <div className="form-inline" key={index}>
               <label>$</label>
               <input type="number" name="name" value={element.name} onChange={e => handleAssetChange(index, e)} />
               {
                 index ? 
-                  <button type="button"  className="remove" onClick={() => removeFormFields(index)}>X</button> 
+                  <button type="button"  className="button remove" onClick={() => removeAssetFields(index)}>X</button> 
                 : null
               }
             </div>
           ))}
           <div className="button-section">
-              <button className="button add" type="button" onClick={() => addFormFields()}>⊕ Add Assets</button>
+              <button className="button add" type="button" onClick={() => addAssetFields()}>Add</button>
           </div>
       </form>
 
